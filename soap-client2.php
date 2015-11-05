9386118c91f2c85946cacd9a74caedba12fc3b39
Alexandr Varabyou <vorobev.profi@gmail.com>
2015-11-05 21:46:25 +0300

    NBRB second SOAP clent DEMO with commentaries

--- soap-client2.php
+++ soap-client2.php
@@ -1,29 +1,8 @@
 <?
-/*$client = new SoapClient("http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?WSDL");
-//$client = new SoapClient("http://www.nbrb.by/Services/ExRates.asmx?wsdl");
-/*echo '<pre>';
-print_r($client->__getFunctions());*/
-//try{
-	// Сколько новостей всего?
-	/*$dateTime = array('On_date' => date('Y-m-d'),);
-	$res = $result->client->GetCursOnDateXML($dateTime);
-	
-	$res->GetCursOnDateXMLResult->any;
-	print_r($res);
-	// Сколько новостей в категории Политика?
-	$result = $client->getNewsCountByCat(1);
-	echo "<p>Всего новостей в категории Политика: $result</p>";
-	
-	// Покажем конкретную новость
-	$result = $client->getNewsById(1);
-	$news = unserialize(base64_decode($result));
-	var_dump($news);*/
-//	}catch(SoapFault $e){
-//		echo 'Операция '.$e->faultcode.' вернула ошибку: '.$e->getMessage();
-//	}
-
+// Ссылка на wsdl документ сервиса Нацбанка Беларуси по курсам валют
 $wsdl = 'http://www.nbrb.by/Services/ExRates.asmx?wsdl';
  
+// Объект SOAP клиента
 $client = new SoapClient($wsdl);
 
 $date = new DateTime(date("Y-m-d"));
@@ -35,7 +14,10 @@ $data = new SimpleXMLElement($result->ExRatesDailyResult->any);
 echo "<pre>";
 print_r($data);
 echo "<pre>";
-foreach ($data->NewDataSet->DailyExRatesOnDate as $curs) {
+/*foreach ($data->NewDataSet->DailyExRatesOnDate as $curs) {
     printf('%s = %s Руб', trim($curs->Cur_QuotName), floatval($curs->Cur_OfficialRate));
     echo PHP_EOL;
+}*/
+foreach ($data->NewDataSet->DailyExRatesOnDate as $cursDollara) {
+    echo $cursDollara->Cur_Abbreviation."\n";
 }
